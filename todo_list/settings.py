55be90b93b1acf18af4fe3d6bfc6810702182028
Raina <1234rainajain@gmail.com>
2022-01-28 20:10:31 +0530

    again commit message

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,5 +123,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
+STATIC_ROOT = os.path.join('static/')
+
+django_heroku.settings(locals())
+
